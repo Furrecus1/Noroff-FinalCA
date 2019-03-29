6a62fbd6bfeb3f8c14d346436e4d4fb441d83b25
Furrecus1 <martin.furre.johnsen@gmail.com>
2019-03-29 03:17:58 +0100

    Add unfinnished app

--- src/pages/about/about.component.ts
+++ src/pages/about/about.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public show:boolean = false;
+  public buttonName:any = 'Show';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggle(){
+    console.log("works");
+    this.show = !this.show;
+
+    if(this.show) {
+      this.buttonName = "Hide"; 
+    } else {
+      this.buttonName = "Show";
+    }
+  }
 }
--- src/pages/contact/contact.component.ts
+++ src/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  messageForm: FormGroup;
+  submitted = false;
+  success = false;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.messageForm = this.formBuilder.group({
+      name:  ['', Validators.required],
+      lastname: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.messageForm.invalid) {
+      return;
+    }
+    this.success = true;
+  }
 
   ngOnInit() {
   }
